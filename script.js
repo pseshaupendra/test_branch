676e3a89f624f43cc4f6659740236c1bf12a78b4
SESHA UPENDRA <137257503+pseshaupendra@users.noreply.github.com>
2023-12-01 20:52:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//button functions
+    //home button
+function scrollToSection(home) {
+var section = document.getElementById(home);
+if (section) {
+    section.scrollIntoView({ behavior: 'smooth'});
+}
+}
+
+  //abstract button
+function scrollToPosition(abstract) {
+var section = document.getElementById(abstract);
+if (section) {
+    section.scrollIntoView({ behavior: 'smooth'});
+}
+}
+
+
+//marquee function control
+    //for images above table
+document.getElementById('img-scroll').addEventListener('mouseover', function() {
+    this.stop();
+});
+document.getElementById('img-scroll').addEventListener('mouseout', function() {
+    this.start();
+});
+    //for images below table
+document.getElementById('img-scroll2').addEventListener('mouseover', function() {
+    this.stop();
+});
+document.getElementById('img-scroll2').addEventListener('mouseout', function() {
+    this.start();
+});
