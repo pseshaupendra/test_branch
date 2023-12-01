0ce3a5248bdac3721d10c2908ca7e24a15d45a9a
SESHA UPENDRA <137257503+pseshaupendra@users.noreply.github.com>
2023-12-01 21:57:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 //button functions
     //home button
 function scrollToSection(home) {
-var section = document.getElementById(home);
-if (section) {
-    section.scrollIntoView({ behavior: 'smooth'});
+    var section = document.getElementById(home);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth'});
+    }
 }
-}
-
-  //abstract button
+    //abstract button
 function scrollToPosition(abstract) {
-var section = document.getElementById(abstract);
-if (section) {
-    section.scrollIntoView({ behavior: 'smooth'});
+    var section = document.getElementById(abstract);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth'});
+    }
 }
+    //csp button
+function scrollToPosition(csp) {
+    var section = document.getElementById(csp);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth'});
+    }
 }
-
+    //contact button
+function scrollToPosition(contact) {
+    var section = document.getElementById(contact);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth'});
+    }
+}
 
 //marquee function control
     //for images above table
